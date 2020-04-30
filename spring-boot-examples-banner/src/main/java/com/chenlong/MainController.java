27162fee7a7fd1a457a3b179fb803c6b2046f69c
陈龙 <chenlong220192@gmail.com>
2020-04-30 13:10:35 +0800

    update

--- spring-boot-examples-banner/src/main/java/com/chenlong/MainController.java
+++ spring-boot-examples-banner/src/main/java/com/chenlong/MainController.java
@@ -12,7 +12,6 @@ public class MainController {
 
     @GetMapping("/")
     public String helloWorld() {
-
         return "Hello World";
     }
 
--- spring-boot-examples-banner/src/main/java/com/chenlong/SpringBootExamplesBannerApplication.java
+++ spring-boot-examples-banner/src/main/java/com/chenlong/SpringBootExamplesBannerApplication.java
@@ -1,6 +1,5 @@
 package com.chenlong;
 
-import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,14 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootExamplesBannerApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(SpringBootStudentBannerApplication.class, args);
-		SpringApplication application = new SpringApplication(SpringBootExamplesBannerApplication.class);
-		/*
-		 * Mode.OFF:关闭;
-		 * Mode.CONSOLE:控制台输出，默认方式;
-		 * Mode.LOG:日志输出方式;
-		 */
-		application.setBannerMode(Mode.CONSOLE);
-		application.run(args);
+		SpringApplication.run(SpringBootExamplesBannerApplication.class, args);
 	}
 }
